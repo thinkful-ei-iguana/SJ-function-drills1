bd5130d87b7b67678d1c23c2db712e2f39010766
schabertdowneyjr <sschab689@gmail.com>
2019-10-21 13:17:28 -0600

    added getYearOfBirth function

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function getYearOfBirth(age) {
+  return 2019 - age;
+}
+
 function createGreeting (name, age){
   const yearOfBirth = 2019 - age;
   return `Hi, my name is ${name} and I'm  ${age} years old`;
