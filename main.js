ae995d58d9c75e7aefe827049eff958330c2a704
schabertdowneyjr <sschab689@gmail.com>
2019-10-21 13:34:55 -0600

    added checks to make sure parameters have been defined

--- main.js
+++ main.js
@@ -11,12 +11,15 @@ function getYearOfBirth(age) {
 
 function createGreeting (name, age){
   const yearOfBirth = 2019 - age;
+  if (name === undefined || age === undefined) {
+    throw new Error ('Arguments not valid');
+  } 
   return `Hi, my name is ${name} and I'm  ${age} years old`;
 }
 
 
 try {
-  const greeting1 = getYearOfBirth(-1);
+  const greeting1 = createGreeting();
   console.log(greeting1);
 } catch(e){
   console.log(e.message);
