40c3efa0064d0cf1105fa05ecf84c9aed523b59a
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-10-21 15:56:14 -0400

    finished functino drills 1

--- main.js
+++ main.js
@@ -10,16 +10,20 @@ function getYearOfBirth(age) {
 }
 
 function createGreeting (name, age){
-  const yearOfBirth = 2019 - age;
+  const yearOfBirth = getYearOfBirth(age);
   if (name === undefined || age === undefined) {
     throw new Error ('Arguments not valid');
-  } 
-  return `Hi, my name is ${name} and I'm  ${age} years old`;
+    // typeof age !== isNan
+  } if(typeof name !== 'string' ||  typeof age !== 'number'){
+    throw new TypeError('name must be a string and age must be a number');
+  }
+  return `Hi, my name is ${name} and I'm  ${age} years old. I was born in ${yearOfBirth}`;
+  
 }
 
 
 try {
-  const greeting1 = createGreeting();
+  const greeting1 = createGreeting('chris',29);
   console.log(greeting1);
 } catch(e){
   console.log(e.message);
